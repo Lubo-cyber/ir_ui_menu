d6ea1fb271f64dded4a936d50f2460e65bd88433
Lubo <proto123wtf@gmail.com>
2025-01-09 09:19:52 +0100

    Merge remote-tracking branch 'CRUD/master'
    # Conflicts:
    #	src/main/java/com/example/crud/view/HelloController.java
    #	src/main/java/com/example/crud/view/HelloController2.java
    #	src/main/java/com/example/crud/view/HelloController3.java

--- src/main/java/com/example/crud/HelloApplication.java
+++ src/main/java/com/example/crud/HelloApplication.java
@@ -7,16 +7,30 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * Incio de aplicacion.
+ * Clase principal de la aplicación que arranca la interfaz gráfica.
+ * Inicia la aplicación JavaFX cargando una vista FXML y aplicando un estilo CSS.
  */
+
 public class HelloApplication extends Application {
 
   /**
-   * El metodo main.
-   * */
+   * Método principal que inicia la aplicación.
+   * Llama al método launch() de la clase Application para iniciar el ciclo de vida de JavaFX.
+   *
+   * @param args Argumentos pasados a la aplicación desde la línea de comandos.
+   */
+
   public static void main(String[] args) {
     launch();
   }
+
+  /**
+   * Método que se ejecuta al iniciar la aplicación.
+   * Carga el archivo FXML, aplica el estilo CSS y configura la ventana principal.
+   *
+   * @param stage El escenario principal de la aplicación.
+   * @throws IOException Si hay un error al cargar el archivo FXML o el CSS.
+   */
 
   @Override
   public void start(Stage stage) throws IOException {
